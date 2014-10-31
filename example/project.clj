4de694a95e4cb1c1993fd5f8eddced350f2fa4f7
Vladislav Bauer <bauer.vlad@gmail.com>
2014-10-31 17:42:15 +0600

    Remove unnecessary clojure dependency, update plugin version in the example project

--- example/project.clj
+++ example/project.clj
@@ -6,7 +6,7 @@
 
 
   ; List of plugins
-  :plugins [[lein-coffeescript "0.1.1"]]
+  :plugins [[lein-coffeescript "0.1.2"]]
 
   ; List of hooks
   ; It's used for running lein-coffeescript during compile phase
--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-glob "1.0.0" :exclusions [org.clojure/clojure]]
+  :dependencies [[clj-glob "1.0.0" :exclusions [org.clojure/clojure]]
                  [lein-npm "0.4.0" :exclusions [org.clojure/clojure]]]
 
   :plugins [[jonase/eastwood "0.1.4" :exclusions [org.clojure/clojure]]
