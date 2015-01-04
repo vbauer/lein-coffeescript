b2881449aa03facaaa7f77152a054652dc3f8f6c
Vladislav Bauer <bauer.vlad@gmail.com>
2015-01-05 04:52:57 +0600

    Update eastwood plugin (0.2.0->0.2.1)

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [lein-npm "0.4.0" :exclusions [org.clojure/clojure]]
                  [me.raynes/fs "1.4.6" :exclusions [org.clojure/clojure]]]
 
-  :plugins [[jonase/eastwood "0.2.0" :exclusions [org.clojure/clojure]]
+  :plugins [[jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
             [lein-kibit "0.0.8" :exclusions [org.clojure/clojure]]
             [lein-bikeshed "0.2.0" :exclusions [org.clojure/clojure]]
             [lein-ancient "0.5.5"]]
