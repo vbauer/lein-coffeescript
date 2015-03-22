617bc3458006642c260ed0b351c484d527d20d77
Vladislav Bauer <bauer.vlad@gmail.com>
2015-03-22 18:50:47 +0600

    Update plugin in the example project

--- example/project.clj
+++ example/project.clj
@@ -6,7 +6,7 @@
 
 
   ; List of plugins
-  :plugins [[lein-coffeescript "0.1.5"]]
+  :plugins [[lein-coffeescript "0.1.6"]]
 
   ; List of hooks
   ; It's used for running lein-coffeescript during compile phase
