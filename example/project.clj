4b6af09011f26705ca2f8e4ec8938e588cf8c4bd
Vladislav Bauer <bauer.vlad@gmail.com>
2015-03-28 14:45:50 +0600

    Update dependency in the example project

--- example/project.clj
+++ example/project.clj
@@ -6,7 +6,7 @@
 
 
   ; List of plugins
-  :plugins [[lein-coffeescript "0.1.6"]]
+  :plugins [[lein-coffeescript "0.1.7"]]
 
   ; List of hooks
   ; It's used for running lein-coffeescript during compile phase
