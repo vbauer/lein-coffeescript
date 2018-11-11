17ef6c591a2242732f562159a3628d2892eadbb1
Vladislav Bauer <vbauer@yandex-team.ru>
2018-11-12 01:29:56 +0700

    0.1.11

--- example/project.clj
+++ example/project.clj
@@ -6,7 +6,7 @@
 
 
   ; List of plugins
-  :plugins [[lein-coffeescript "0.1.9"]]
+  :plugins [[lein-coffeescript "0.1.11"]]
 
   ; List of hooks
   ; It's used for running lein-coffeescript during compile phase
